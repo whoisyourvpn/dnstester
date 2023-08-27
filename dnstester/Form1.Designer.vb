d74a7579acc5c2652b41f2600046d7b26863efe1
jorda <jorda@CAYDE-6>
2023-08-27 16:39:34 -0700

    Add project files.

---
+++ dnstester/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblDnsServer = New Label()
+        cmbDnsServers = New ComboBox()
+        btnQuery = New Button()
+        txtOutput = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' lblDnsServer
+        ' 
+        lblDnsServer.AutoSize = True
+        lblDnsServer.Location = New Point(37, 21)
+        lblDnsServer.Name = "lblDnsServer"
+        lblDnsServer.Size = New Size(169, 25)
+        lblDnsServer.TabIndex = 1
+        lblDnsServer.Text = "Select a DNS server:"
+        ' 
+        ' cmbDnsServers
+        ' 
+        cmbDnsServers.FormattingEnabled = True
+        cmbDnsServers.Location = New Point(212, 18)
+        cmbDnsServers.Name = "cmbDnsServers"
+        cmbDnsServers.Size = New Size(297, 33)
+        cmbDnsServers.TabIndex = 2
+        ' 
+        ' btnQuery
+        ' 
+        btnQuery.Location = New Point(397, 66)
+        btnQuery.Name = "btnQuery"
+        btnQuery.Size = New Size(112, 34)
+        btnQuery.TabIndex = 3
+        btnQuery.Text = "Run test"
+        btnQuery.UseVisualStyleBackColor = True
+        ' 
+        ' txtOutput
+        ' 
+        txtOutput.Location = New Point(56, 133)
+        txtOutput.Name = "txtOutput"
+        txtOutput.ReadOnly = True
+        txtOutput.Size = New Size(453, 287)
+        txtOutput.TabIndex = 4
+        txtOutput.Text = ""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(633, 541)
+        Controls.Add(txtOutput)
+        Controls.Add(btnQuery)
+        Controls.Add(cmbDnsServers)
+        Controls.Add(lblDnsServer)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblDnsServer As Label
+    Friend WithEvents cmbDnsServers As ComboBox
+    Friend WithEvents btnQuery As Button
+    Friend WithEvents txtOutput As RichTextBox
+End Class
---
+++ dnstester/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net
+Imports DnsClient
+Imports System.Diagnostics ' Required for Stopwatch
+
+Public Class Form1
+
+    ' List of preconfigured domains
+    Private domainsToTest As List(Of String) = New List(Of String)() From {
+        "google.com",
+        "facebook.com",
+        "twitter.com",
+        "microsoft.com",
+        "apple.com"
+    }
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Fill the ComboBox with predefined DNS servers
+        cmbDnsServers.Items.Add("8.8.8.8") ' Google
+        cmbDnsServers.Items.Add("1.1.1.1") ' Cloudflare
+        cmbDnsServers.Items.Add("208.67.222.222") ' OpenDNS
+        '... add more if necessary ...
+    End Sub
+
+    Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
+        Dim dnsServer As String = cmbDnsServers.Text.Trim()
+        If String.IsNullOrEmpty(dnsServer) Then
+            MessageBox.Show("Please select or enter a DNS server.")
+            Return
+        End If
+
+        Dim lookupClient As New LookupClient(IPAddress.Parse(dnsServer))
+        txtOutput.Clear()
+
+        Dim totalSuccessfulResolutions As Integer = 0
+        Dim totalTime As Double = 0
+
+        For Each domain In domainsToTest
+            Dim success As Boolean = False
+            For i As Integer = 1 To 5 ' We'll test each domain 5 times
+                Try
+                    Dim watch As Stopwatch = Stopwatch.StartNew() ' Start timing
+                    Dim result = lookupClient.Query(domain, QueryType.A)
+                    watch.Stop() ' Stop timing
+
+                    If result.Answers.Count > 0 Then
+                        totalTime += watch.Elapsed.TotalMilliseconds ' Add the time it took for this resolution
+                        totalSuccessfulResolutions += 1
+                        success = True
+                        Exit For ' Exit the loop once we get a successful resolution
+                    End If
+                Catch ex As Exception
+                    ' If there's an error, just continue to the next iteration
+                End Try
+            Next
+
+            If success Then
+                txtOutput.AppendText($"{domain}........ok{Environment.NewLine}")
+            Else
+                txtOutput.AppendText($"{domain}........timed out{Environment.NewLine}")
+            End If
+        Next
+
+        Dim averageTime As Double = totalTime / totalSuccessfulResolutions ' Calculate average time for successful resolutions
+        txtOutput.AppendText($"Average speed: {averageTime:0.##}ms{Environment.NewLine}")
+    End Sub
+
+End Class
