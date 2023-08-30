4e4150284601a3a86b5ad45b7e23d709f75d1034
skarz <jorda@CAYDE-6>
2023-08-29 18:08:27 -0700

    update

--- dnstester/Form1.Designer.vb
+++ dnstester/Form1.Designer.vb
@@ -22,64 +22,130 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        lblDnsServer = New Label()
-        cmbDnsServers = New ComboBox()
-        btnQuery = New Button()
+        Home = New TabControl()
+        TabPage1 = New TabPage()
+        btnTestAll = New Button()
         txtOutput = New RichTextBox()
+        btnQuery = New Button()
+        cmbDnsServers = New ComboBox()
+        lblDnsServer = New Label()
+        Results = New TabPage()
+        rtbResultsLog = New RichTextBox()
+        Home.SuspendLayout()
+        TabPage1.SuspendLayout()
+        Results.SuspendLayout()
         SuspendLayout()
         ' 
-        ' lblDnsServer
+        ' Home
         ' 
-        lblDnsServer.AutoSize = True
-        lblDnsServer.Location = New Point(37, 21)
-        lblDnsServer.Name = "lblDnsServer"
-        lblDnsServer.Size = New Size(169, 25)
-        lblDnsServer.TabIndex = 1
-        lblDnsServer.Text = "Select a DNS server:"
+        Home.Controls.Add(TabPage1)
+        Home.Controls.Add(Results)
+        Home.Location = New Point(0, 0)
+        Home.Name = "Home"
+        Home.SelectedIndex = 0
+        Home.Size = New Size(621, 529)
+        Home.TabIndex = 6
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(btnTestAll)
+        TabPage1.Controls.Add(txtOutput)
+        TabPage1.Controls.Add(btnQuery)
+        TabPage1.Controls.Add(cmbDnsServers)
+        TabPage1.Controls.Add(lblDnsServer)
+        TabPage1.Location = New Point(4, 34)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(613, 491)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Home"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' btnTestAll
+        ' 
+        btnTestAll.Location = New Point(274, 92)
+        btnTestAll.Name = "btnTestAll"
+        btnTestAll.Size = New Size(112, 34)
+        btnTestAll.TabIndex = 10
+        btnTestAll.Text = "Test all"
+        btnTestAll.UseVisualStyleBackColor = True
+        ' 
+        ' txtOutput
+        ' 
+        txtOutput.Location = New Point(51, 159)
+        txtOutput.Name = "txtOutput"
+        txtOutput.ReadOnly = True
+        txtOutput.Size = New Size(529, 287)
+        txtOutput.TabIndex = 9
+        txtOutput.Text = ""
+        ' 
+        ' btnQuery
+        ' 
+        btnQuery.Location = New Point(392, 92)
+        btnQuery.Name = "btnQuery"
+        btnQuery.Size = New Size(112, 34)
+        btnQuery.TabIndex = 8
+        btnQuery.Text = "Run test"
+        btnQuery.UseVisualStyleBackColor = True
         ' 
         ' cmbDnsServers
         ' 
         cmbDnsServers.FormattingEnabled = True
-        cmbDnsServers.Location = New Point(212, 18)
+        cmbDnsServers.Location = New Point(207, 44)
         cmbDnsServers.Name = "cmbDnsServers"
         cmbDnsServers.Size = New Size(297, 33)
-        cmbDnsServers.TabIndex = 2
+        cmbDnsServers.TabIndex = 7
         ' 
-        ' btnQuery
+        ' lblDnsServer
         ' 
-        btnQuery.Location = New Point(397, 66)
-        btnQuery.Name = "btnQuery"
-        btnQuery.Size = New Size(112, 34)
-        btnQuery.TabIndex = 3
-        btnQuery.Text = "Run test"
-        btnQuery.UseVisualStyleBackColor = True
+        lblDnsServer.AutoSize = True
+        lblDnsServer.Location = New Point(32, 47)
+        lblDnsServer.Name = "lblDnsServer"
+        lblDnsServer.Size = New Size(169, 25)
+        lblDnsServer.TabIndex = 6
+        lblDnsServer.Text = "Select a DNS server:"
         ' 
-        ' txtOutput
+        ' Results
         ' 
-        txtOutput.Location = New Point(56, 133)
-        txtOutput.Name = "txtOutput"
-        txtOutput.ReadOnly = True
-        txtOutput.Size = New Size(453, 287)
-        txtOutput.TabIndex = 4
-        txtOutput.Text = ""
+        Results.Controls.Add(rtbResultsLog)
+        Results.Location = New Point(4, 34)
+        Results.Name = "Results"
+        Results.Padding = New Padding(3)
+        Results.Size = New Size(613, 491)
+        Results.TabIndex = 1
+        Results.Text = "Results"
+        Results.UseVisualStyleBackColor = True
+        ' 
+        ' rtbResultsLog
+        ' 
+        rtbResultsLog.Location = New Point(8, 6)
+        rtbResultsLog.Name = "rtbResultsLog"
+        rtbResultsLog.ScrollBars = RichTextBoxScrollBars.Vertical
+        rtbResultsLog.Size = New Size(599, 479)
+        rtbResultsLog.TabIndex = 0
+        rtbResultsLog.Text = ""
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(633, 541)
-        Controls.Add(txtOutput)
-        Controls.Add(btnQuery)
-        Controls.Add(cmbDnsServers)
-        Controls.Add(lblDnsServer)
+        Controls.Add(Home)
         Name = "Form1"
         Text = "Form1"
+        Home.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage1.PerformLayout()
+        Results.ResumeLayout(False)
         ResumeLayout(False)
-        PerformLayout()
     End Sub
-
-    Friend WithEvents lblDnsServer As Label
-    Friend WithEvents cmbDnsServers As ComboBox
-    Friend WithEvents btnQuery As Button
+    Friend WithEvents Home As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Results As TabPage
+    Friend WithEvents btnTestAll As Button
     Friend WithEvents txtOutput As RichTextBox
+    Friend WithEvents btnQuery As Button
+    Friend WithEvents cmbDnsServers As ComboBox
+    Friend WithEvents lblDnsServer As Label
+    Friend WithEvents rtbResultsLog As RichTextBox
 End Class
--- dnstester/Form1.vb
+++ dnstester/Form1.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.Net
 Imports DnsClient
 Imports System.Diagnostics ' Required for Stopwatch
+Imports System.IO
+Imports System.Xml.Serialization
+
 
 Public Class Form1
 
@@ -13,20 +16,44 @@ Public Class Form1
         "apple.com"
     }
 
+    ' Dictionary of DNS servers and their associated names
+    Private dnsServers As Dictionary(Of String, String) = New Dictionary(Of String, String)() From {
+        {"1.1.1.1", "Cloudflare"},
+        {"8.8.8.8", "Google"},
+        {"45.90.28.180", "NextDNS"},
+        {"9.9.9.9", "Quad9"},
+        {"208.67.222.222", "OpenDNS"},
+        {"94.140.14.14", "Adguard"},
+        {"77.88.8.8", "Yandex DNS"},
+        {"8.26.56.26", "Comodo Secure DNS"},
+        {"76.76.2.0", "ControlD"}
+    }
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Fill the ComboBox with predefined DNS servers
-        cmbDnsServers.Items.Add("8.8.8.8") ' Google
-        cmbDnsServers.Items.Add("1.1.1.1") ' Cloudflare
-        cmbDnsServers.Items.Add("208.67.222.222") ' OpenDNS
-        '... add more if necessary ...
+        For Each kvp In dnsServers
+            cmbDnsServers.Items.Add($"{kvp.Key} ({kvp.Value})")
+        Next
     End Sub
 
+    Public Class DnsTestResult
+        Public Property Server As String
+        Public Property Name As String
+        Public Property AverageSpeed As Double
+    End Class
+
+
     Private Sub btnQuery_Click(sender As Object, e As EventArgs) Handles btnQuery.Click
-        Dim dnsServer As String = cmbDnsServers.Text.Trim()
-        If String.IsNullOrEmpty(dnsServer) Then
+        Dim selectedItem As String = cmbDnsServers.Text.Trim()
+        If String.IsNullOrEmpty(selectedItem) Then
             MessageBox.Show("Please select or enter a DNS server.")
             Return
         End If
+
+        ' Extract just the IP from the selected item for the DNS lookup
+        Dim dnsServer As String = selectedItem.Split(" ")(0)
+
+        Dim serverName As String = dnsServers(dnsServer)
 
         Dim lookupClient As New LookupClient(IPAddress.Parse(dnsServer))
         txtOutput.Clear()
@@ -62,6 +89,102 @@ Public Class Form1
 
         Dim averageTime As Double = totalTime / totalSuccessfulResolutions ' Calculate average time for successful resolutions
         txtOutput.AppendText($"Average speed: {averageTime:0.##}ms{Environment.NewLine}")
+
+        Dim newResult As New DnsTestResult With {
+    .Server = dnsServer,
+    .Name = serverName,
+    .AverageSpeed = averageTime
+}
+
+        Dim currentResults As List(Of DnsTestResult) = LoadResults()
+        currentResults.Add(newResult)
+        SaveResults(currentResults)
+
     End Sub
+
+    Private Sub btnTestAll_Click(sender As Object, e As EventArgs)
+        txtOutput.Clear()
+
+        ' Estimation of the maximum width for the speed results.
+        ' This includes the server's IP address, name, and the format ":.................".
+        Dim estimatedMaxLengthWithoutMs = dnsServers.Max(Function(kvp) kvp.Key.Length + kvp.Value.Length + 3)
+
+        For Each kvp In dnsServers
+            Dim dnsServer = kvp.Key
+            Dim serverName = kvp.Value
+            Dim label As String = $"{dnsServer} ({serverName}):"
+
+            Dim lookupClient As New LookupClient(IPAddress.Parse(dnsServer))
+
+            Dim totalSuccessfulResolutions As Integer = 0
+            Dim totalTime As Double = 0
+
+            For Each domain In domainsToTest
+                For i As Integer = 1 To 5 ' We'll test each domain 5 times
+                    Try
+                        Dim watch As Stopwatch = Stopwatch.StartNew() ' Start timing
+                        Dim result = lookupClient.Query(domain, QueryType.A)
+                        watch.Stop() ' Stop timing
+
+                        If result.Answers.Count > 0 Then
+                            totalTime += watch.Elapsed.TotalMilliseconds ' Add the time it took for this resolution
+                            totalSuccessfulResolutions += 1
+                            Exit For ' Exit the loop once we get a successful resolution
+                        End If
+                    Catch ex As Exception
+                        ' If there's an error, just continue to the next iteration
+                    End Try
+                Next
+            Next
+
+            ' Update the user with the test result for the DNS server
+            If totalSuccessfulResolutions > 0 Then
+                Dim averageTime As Double = totalTime / totalSuccessfulResolutions ' Calculate average time for successful resolutions
+                Dim averageTimeString As String = $"{averageTime:0.##}ms"
+
+                Dim totalCharsForDots = estimatedMaxLengthWithoutMs - label.Length
+                Dim dotsString = New String("."c, totalCharsForDots)
+                txtOutput.AppendText($"{label}{dotsString}{averageTimeString}{Environment.NewLine}")
+            Else
+                Dim totalCharsForDots = estimatedMaxLengthWithoutMs - label.Length
+                Dim dotsString = New String("."c, totalCharsForDots)
+                txtOutput.AppendText($"{label}{dotsString}Timed out{Environment.NewLine}")
+            End If
+        Next
+    End Sub
+
+    Public Sub SaveResults(results As List(Of DnsTestResult))
+        Dim serializer As New Xml.Serialization.XmlSerializer(GetType(List(Of DnsTestResult)))
+        Using writer As New StreamWriter("results.xml")
+            serializer.Serialize(writer, results)
+        End Using
+    End Sub
+
+    Public Function LoadResults() As List(Of DnsTestResult)
+        If File.Exists("results.xml") Then
+            Dim serializer As New Xml.Serialization.XmlSerializer(GetType(List(Of DnsTestResult)))
+            Using reader As New StreamReader("results.xml")
+                Return CType(serializer.Deserialize(reader), List(Of DnsTestResult))
+            End Using
+        Else
+            Return New List(Of DnsTestResult)()
+        End If
+    End Function
+
+    Public Sub DisplayResults()
+        Dim results As List(Of DnsTestResult) = LoadResults()
+        rtbResultsLog.Clear()
+        For Each result In results
+            rtbResultsLog.AppendText($"{result.Server} ({result.Name}): {result.AverageSpeed:0.##}ms{Environment.NewLine}")
+        Next
+    End Sub
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Home.SelectedIndexChanged
+        If Home.SelectedIndex = 1 Then ' Assuming the second tab's index is 1 (0-based index)
+            DisplayResults()
+        End If
+    End Sub
+
+
 
 End Class
